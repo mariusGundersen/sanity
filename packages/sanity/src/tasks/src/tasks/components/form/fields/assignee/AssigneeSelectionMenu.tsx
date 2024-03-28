a2a2ab1e7fa03252cda483f6e4cb737fc20b6dec
Pedro Bonamin <pedrobonamin@gmail.com>
2024-03-28 12:01:01 +0100

    feat(tasks): add tasks subscribers menu

--- packages/sanity/src/tasks/src/tasks/components/form/fields/assignee/AssigneeSelectionMenu.tsx
+++ packages/sanity/src/tasks/src/tasks/components/form/fields/assignee/AssigneeSelectionMenu.tsx
@@ -150,7 +150,7 @@ function MentionsMenu({onSelect, value = ''}: {onSelect: SelectItemHandler; valu
   return (
     <div onKeyDown={handleKeyDown}>
       <TextInput
-        placeholder={t('form.input.assignee.search.placeholder')}
+        placeholder={t('form.subscribers.menu.input.placeholder')}
         autoFocus
         border={false}
         onChange={handleSearchChange}
@@ -162,7 +162,7 @@ function MentionsMenu({onSelect, value = ''}: {onSelect: SelectItemHandler; valu
         autoComplete="off"
       />
 
-      <div style={{maxHeight: '320px', overflowY: 'scroll', paddingTop: '8px'}}>
+      <div style={{maxHeight: '320px', overflowY: 'scroll', paddingTop: '8px', width: '100%'}}>
         {filteredOptions.length === 0 ? (
           <Box padding={3}>
             <Text align="center" size={1} muted>
