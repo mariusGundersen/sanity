a55f6602c8bea7c900f90c93bbace575bfb698b1
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-06-05 12:05:09 +0200

    chore(page-building): test new array insert menu options

--- dev/page-building-studio/schemaTypes/page.ts
+++ dev/page-building-studio/schemaTypes/page.ts
@@ -15,6 +15,55 @@ export const page = defineType({
       title: 'Blocks',
       type: 'array',
       of: [{type: 'hero'}, {type: 'logo-carousel'}, {type: 'testimonials'}],
+      options: {
+        insertMenu: {
+          filter: true,
+          views: ['grid', 'list'],
+          groups: [
+            {
+              name: 'intro',
+              title: 'Intro',
+              of: ['hero'],
+            },
+            {
+              name: 'storytelling',
+              title: 'Storytelling',
+            },
+            {
+              name: 'upsell',
+              title: 'Upsell',
+              of: ['testimonials', 'hero'],
+            },
+          ],
+        },
+      },
+    }),
+  ],
+})
+
+export const pageOneBlockType = defineType({
+  type: 'document',
+  name: 'pageOneBlockType',
+  title: 'Page (One Block Type)',
+  groups: [{name: 'meta'}, {name: 'blocks'}],
+  fields: [
+    defineField({
+      type: 'string',
+      name: 'title',
+      title: 'Title',
+      group: 'meta',
+    }),
+    defineField({
+      name: 'blocks',
+      title: 'Blocks',
+      type: 'array',
+      group: 'blocks',
+      of: [{type: 'hero'}],
+      options: {
+        insertMenu: {
+          views: ['grid', 'list'],
+        },
+      },
     }),
   ],
 })
